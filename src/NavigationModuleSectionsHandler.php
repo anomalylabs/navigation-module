7d93f9afb3af3e1dd37b4919d02c5b267ad04bda
Osvaldo Brignoni <obrignoni@gmail.com>
2015-04-09 15:26:29 -0500

    Simpler

--- src/NavigationModuleSectionsHandler.php
+++ src/NavigationModuleSectionsHandler.php
@@ -6,41 +6,38 @@ use Illuminate\Foundation\Bus\DispatchesCommands;
 
 /**
  * Class NavigationModuleSectionsHandler
+ *
  * @package Anomaly\NavigationModule
  */
 class NavigationModuleSectionsHandler
 {
+
     use DispatchesCommands;
 
     public function handle(ControlPanelBuilder $builder)
     {
-        $buttons = [];
-
-        $linkTypeButtons = $this->dispatch(new GetLinkTypeButtons());
-
-        if ($linkTypeButtons) {
-            $buttons[] = [
-                'button'   => 'new',
-                'text'     => 'New Link',
-                'dropdown' => $linkTypeButtons,
-            ];
-        }
-
-/*        $buttons[] = [
-            'button' => 'new',
-            'text'   => 'New Group',
-            'href'   => route('admin.navigation.groups.create'),
-        ];*/
-
-        $builder->setSections([
-            'links'  => [
-                'href'    => route('admin.navigation'),
-                'buttons' => $buttons
-            ],
-            'groups' => [
-                'buttons' => $buttons
-            ],
-        ]);
+        $builder->setSections(
+            [
+                'links'  => [
+                    'href'    => route('admin.navigation'),
+                    'buttons' => [
+                        [
+                            'button'   => 'new',
+                            'text'     => 'New Link',
+                            'dropdown' => $this->dispatch(new GetLinkTypeButtons()),
+                        ]
+                    ]
+                ],
+                'groups' => [
+                    'buttons' => [
+                        [
+                            'button' => 'new',
+                            'text'   => 'New Group',
+                            'href'   => route('admin.navigation.groups.create'),
+                        ]
+                    ]
+                ],
+            ]
+        );
     }
-
 }
