0544582904cabd271ee3b961d0ab719efa8b6acd
Ryan Thompson <service@aiwebsystems.com>
2016-06-22 01:17:29 -0500

    Fix delete issue - 404

--- src/NavigationModuleServiceProvider.php
+++ src/NavigationModuleServiceProvider.php
@@ -28,16 +28,16 @@ class NavigationModuleServiceProvider extends AddonServiceProvider
      * @var array
      */
     protected $routes = [
-        'admin/navigation'                          => 'Anomaly\NavigationModule\Http\Controller\Admin\MenusController@index',
-        'admin/navigation/choose'                   => 'Anomaly\NavigationModule\Http\Controller\Admin\MenusController@choose',
-        'admin/navigation/create'                   => 'Anomaly\NavigationModule\Http\Controller\Admin\MenusController@create',
-        'admin/navigation/edit/{id}'                => 'Anomaly\NavigationModule\Http\Controller\Admin\MenusController@edit',
-        'admin/navigation/links/{menu?}'            => 'Anomaly\NavigationModule\Http\Controller\Admin\LinksController@index',
-        'admin/navigation/links/{menu}/create'      => 'Anomaly\NavigationModule\Http\Controller\Admin\LinksController@create',
-        'admin/navigation/links/{menu}/edit/{id}'   => 'Anomaly\NavigationModule\Http\Controller\Admin\LinksController@edit',
-        'admin/navigation/links/{menu}/view/{id}'   => 'Anomaly\NavigationModule\Http\Controller\Admin\LinksController@view',
-        'admin/navigation/links/{menu}/delete/{id}' => 'Anomaly\NavigationModule\Http\Controller\Admin\LinksController@delete',
-        'admin/navigation/links/choose/{menu}'      => 'Anomaly\NavigationModule\Http\Controller\Admin\LinksController@choose'
+        'admin/navigation'                        => 'Anomaly\NavigationModule\Http\Controller\Admin\MenusController@index',
+        'admin/navigation/choose'                 => 'Anomaly\NavigationModule\Http\Controller\Admin\MenusController@choose',
+        'admin/navigation/create'                 => 'Anomaly\NavigationModule\Http\Controller\Admin\MenusController@create',
+        'admin/navigation/edit/{id}'              => 'Anomaly\NavigationModule\Http\Controller\Admin\MenusController@edit',
+        'admin/navigation/links/{menu?}'          => 'Anomaly\NavigationModule\Http\Controller\Admin\LinksController@index',
+        'admin/navigation/links/{menu}/create'    => 'Anomaly\NavigationModule\Http\Controller\Admin\LinksController@create',
+        'admin/navigation/links/{menu}/edit/{id}' => 'Anomaly\NavigationModule\Http\Controller\Admin\LinksController@edit',
+        'admin/navigation/links/{menu}/view/{id}' => 'Anomaly\NavigationModule\Http\Controller\Admin\LinksController@view',
+        'admin/navigation/links/delete/{id}'      => 'Anomaly\NavigationModule\Http\Controller\Admin\LinksController@delete',
+        'admin/navigation/links/choose/{menu}'    => 'Anomaly\NavigationModule\Http\Controller\Admin\LinksController@choose'
     ];
 
     /**
