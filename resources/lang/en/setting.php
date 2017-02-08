6a8e00bc95a52c1a29e91c22ad5e179e7dc3ca00
Denis Efremov <efremov.a.denis@gmail.com>
2017-02-09 01:31:56 +0300

    Create setting.php

---
+++ resources/lang/en/setting.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'enable_hover_mode' => [
+        'name'         => 'Enable Hover Menu Mode',
+        'instructions' => 'Enables on hover menu slide effect to show titles.',
+        'warning'      => 'Be carefull! It might be annoying for you!',
+    ],
+];
