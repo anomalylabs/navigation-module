284c30c643a5e253a2179fa27645eeedc06932f5
Denis Efremov <efremov.a.denis@gmail.com>
2017-02-09 01:31:02 +0300

    Create settings.php

---
+++ resources/config/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'enable_hover_mode'      => [
+        'type'     => 'anomaly.field_type.boolean',
+        'required' => false,
+        'config'   => [
+            'default_value' => false,
+        ],
+    ],
+];
