073c0d358b7145d2e71b55b998907e8ddac1cb05
Jaroslav Petrusevic <jaroslav@webas.lt>
2017-08-28 10:09:16 +0300

    add LT translation

---
+++ resources/lang/lt/message.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'broken' => 'Sugadintas',
+    'restricted' => 'Ribotas',
+    'external' => 'Linkas į iššorę',
+    'choose_link_type' => 'Kokio tipo linką norite sukurti?',
+    'choose_menu' => 'Kurio meniu linkus norėtumėte valdyti?',
+];
---
+++ resources/lang/lt/permission.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'menus' => [
+        'name' => 'Meniu',
+        'option' => [
+            'read' => 'Prieiga prie meniu skyriaus.',
+            'write' => 'Gali kurti ir redaguoti meniu.',
+            'delete' => 'Gali ištrinti meniu.',
+        ],
+    ],
+    'links' => [
+        'name' => 'Linkai',
+        'option' => [
+            'read' => 'Prieiga prie linkų skyriaus',
+            'write' => 'Gali sukurti ir redaguoti linkus.',
+            'delete' => 'Gali ištrinti linkus.',
+        ],
+    ],
+];
