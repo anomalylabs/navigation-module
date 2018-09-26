77e5c5802076329bd3b4b5775c5e21aaa1ac4f7c
Ryan Thompson <service@aiwebsystems.com>
2018-09-26 15:36:50 -0500

    Lookup and relations table views

---
+++ src/Link/Support/MultipleFieldType/ValueTableBuilder.php
@@ -0,0 +1,50 @@
+<?php namespace Anomaly\NavigationModule\Link\Support\MultipleFieldType;
+
+/**
+ * Class ValueTableBuilder
+ *
+ * @link          http://pyrocms.com/
+ * @author        PyroCMS, Inc. <support@pyrocms.com>
+ * @author        Ryan Thompson <ryan@pyrocms.com>
+ */
+class ValueTableBuilder extends \Anomaly\MultipleFieldType\Table\ValueTableBuilder
+{
+
+    /**
+     *
+     * @var array
+     */
+    protected $filters = [
+        'menu',
+        'target',
+    ];
+
+    /**
+     * The table columns.
+     *
+     * @var array
+     */
+    protected $columns = [
+        'link' => [
+            'sort_column' => 'title',
+            'wrapper'     => '
+                    <strong>{value.title}</strong>
+                    <br>
+                    <small class="text-muted">{value.url}</small>',
+            'value'       => [
+                'url'   => 'entry.url',
+                'title' => 'entry.title',
+            ],
+        ],
+        'entry.type.title',
+        'menu',
+    ];
+
+    protected $options = [
+        'order_by' => [
+            'parent_id'  => 'ASC',
+            'sort_order' => 'ASC',
+        ],
+    ];
+
+}
