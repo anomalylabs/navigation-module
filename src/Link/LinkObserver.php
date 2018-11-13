90322424a7dbc1a2029977554e8d6023f198c9dd
Ryan Thompson <service@aiwebsystems.com>
2018-11-13 13:27:41 -0600

    Renamed

--- src/Link/LinkObserver.php
+++ src/Link/LinkObserver.php
@@ -1,9 +1,9 @@
 <?php namespace Anomaly\NavigationModule\Link;
 
 use Anomaly\NavigationModule\Link\Contract\LinkInterface;
-use Anomaly\Streams\Platform\Entry\Command\ClearHttpCache;
 use Anomaly\Streams\Platform\Entry\Contract\EntryInterface;
 use Anomaly\Streams\Platform\Entry\EntryObserver;
+use Anomaly\Streams\Platform\Http\Command\ClearHttpCache;
 
 /**
  * Class LinkObserver
@@ -22,7 +22,7 @@ class LinkObserver extends EntryObserver
      */
     public function saved(EntryInterface $entry)
     {
-        $this->dispatch(new ClearHttpCache($entry));
+        $this->dispatch(new ClearHttpCache());
 
         return parent::saved($entry);
     }
--- src/Menu/MenuObserver.php
+++ src/Menu/MenuObserver.php
@@ -1,9 +1,9 @@
 <?php namespace Anomaly\NavigationModule\Menu;
 
 use Anomaly\NavigationModule\Menu\Contract\MenuInterface;
-use Anomaly\Streams\Platform\Entry\Command\ClearHttpCache;
 use Anomaly\Streams\Platform\Entry\Contract\EntryInterface;
 use Anomaly\Streams\Platform\Entry\EntryObserver;
+use Anomaly\Streams\Platform\Http\Command\ClearHttpCache;
 
 /**
  * Class MenuObserver
@@ -22,7 +22,7 @@ class MenuObserver extends EntryObserver
      */
     public function saved(EntryInterface $entry)
     {
-        $this->dispatch(new ClearHttpCache($entry));
+        $this->dispatch(new ClearHttpCache());
 
         return parent::saved($entry);
     }
