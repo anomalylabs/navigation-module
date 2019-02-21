8055c5f0a119c02825269feb27d98612bd37928e
Ryan Thompson <ryan@pyrocms.com>
2019-02-21 14:30:04 -0600

    Docs

--- src/Link/Form/LinkFormBuilder.php
+++ src/Link/Form/LinkFormBuilder.php
@@ -8,9 +8,9 @@ use Anomaly\Streams\Platform\Ui\Form\FormBuilder;
 /**
  * Class LinkFormBuilder
  *
- * @link          http://pyrocms.com/
- * @author        PyroCMS, Inc. <support@pyrocms.com>
- * @author        Ryan Thompson <ryan@pyrocms.com>
+ * @link   http://pyrocms.com/
+ * @author PyroCMS, Inc. <support@pyrocms.com>
+ * @author Ryan Thompson <ryan@pyrocms.com>
  */
 class LinkFormBuilder extends FormBuilder
 {
